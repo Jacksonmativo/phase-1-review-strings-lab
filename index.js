1cfc611c4b29a4a1edf97060282edb0b2ed2ba01
jackson mativo <jacksonmativo21@gmail.com>
2025-03-10 09:46:08 -0400

    Completed string manipulation lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// Define the current user
+const currentUser = 'Grace Hopper';
+
+// Create a welcome message using string interpolation
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Create an excited version of the welcome message
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Create a short greeting using the first initial of the current user
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
